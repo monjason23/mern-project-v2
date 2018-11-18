5ec5fc2aee87d2dcdf516d85fbf9bfaa92375d70
monjason23 <mfabico@mfabicos-MacBook-Pro.local>
2018-11-18 23:57:20 +0800

    Testing

--- server/server.js
+++ server/server.js
@@ -16,10 +16,10 @@ var mongoose = require("./db/mongoose");
 
 //Config
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "client/build")));
+  app.use(express.static(path.resolve(__dirname, "client/building")));
 
   app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/building", "index.html"));
   });
 }
 
