44f25f45b362c0841bbd85a4cf2a15812ea3fb48
monjason23 <mfabico@mfabicos-MacBook-Pro.local>
2018-11-18 23:48:50 +0800

    Edited file path

--- server/config/production.js
+++ server/config/production.js
@@ -3,10 +3,10 @@ var express = require("express");
 
 module.exports = function(app) {
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "client/build")));
+    app.use(express.static(path.resolve(__dirname, "build")));
 
     app.get("*", function(req, res) {
-      res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "build", "index.html"));
     });
   }
 };
